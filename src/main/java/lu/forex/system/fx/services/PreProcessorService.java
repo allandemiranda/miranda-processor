6f172f9bbc774c6cf877aeab9610c13da4286cba
Allan de Miranda Silva <allandemiranda@gmail.com>
2024-07-30 11:34:05 +0200

    update plus one

--- src/main/java/lu/forex/system/fx/services/PreProcessorService.java
+++ src/main/java/lu/forex/system/fx/services/PreProcessorService.java
@@ -2,10 +2,13 @@ package lu.forex.system.fx.services;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lu.forex.system.fx.models.Tick;
 import lu.forex.system.fx.models.Trade;
 import lu.forex.system.fx.providers.PreProcessorProvider;
 import lu.forex.system.fx.repository.CandlestickRepository;
@@ -25,10 +28,14 @@ public class PreProcessorService implements PreProcessorProvider {
 
   @Override
   public void startDataSystem(final @NonNull File inputFolder) {
+    final AtomicReference<Tick> lastTick = new AtomicReference<>();
     PreProcessor.getExternalizingCollection(inputFolder).forEach((tick, candlesticks) -> {
-      this.getTickRepository().save(tick);
+      lastTick.set(tick);
       this.getCandlestickRepository().saveAll(candlesticks);
     });
+    if (Objects.nonNull(lastTick.get())) {
+      this.getTickRepository().save(lastTick.get());
+    }
   }
 
   @Override
